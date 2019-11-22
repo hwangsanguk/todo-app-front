94771015b2eb2e327d72fceed5c7ba40364b3056
tony <lall21@naver.com>
2019-11-22 11:36:10 +0900

    [ADD]:POST 추가

--- todo-app/src/components/App.js
+++ todo-app/src/components/App.js
@@ -8,20 +8,39 @@ const App = () =>{
 
     //GET
     const getTodos = async () =>{
-        console.log('todo list 가져오기');
         const result = await Axios.get(url);
         const { data } = result;
         setTodos(data);
     };
 
+    //POST
+    const postTodos = async () =>{
+        console.log('todo작성 시작');
+        const sample = {
+            //TODO: UNIQ. KEY
+            id : todos.length +1,
+            content: "react.js"
+        };
+        const result = await Axios.post(url, sample);
+        const {data} = result;
+        //이렇게만 하면 sample을 front에 보여주지 못함
+
+        //setTodos를 통해서 state값을 조정하여 새로 랜더링
+        setTodos([...todos,data]);
+    };
+
     //react가 mounte 되었을때 딱 한번 불러온다
     useEffect(()=> {
         getTodos();
-    },[]);
+    },[]); //빈 배열을 넣어주어야 1회만 불러줌
 
     return(
         <>
-        <h1>front</h1>
+        <h1>Todo front</h1>
+        <form>
+            <input type="text" />
+            <button type="button" onClick={postTodos}>등록</button>
+        </form>
         {todos ?
          todos.map(el =>(<div key={el.id}>{el.content}</div>))
           : <div>Spinner</div>}
